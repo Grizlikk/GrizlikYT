f5444b990d77f0736f80d261270ead0239a83cce
Grizlik :D <96445475+Grizlikk@users.noreply.github.com>
2022-07-06 21:28:02 +0200

    Create Program 3.bat

---
+++ Batch/Batch #02 Základní příkazy/Program 3.bat
@@ -0,0 +1,20 @@
+@echo off
+echo 1
+timeout /t 1
+echo 2
+timeout /t 1
+echo 3
+timeout /t 1
+echo 4
+timeout /t 1
+echo 5
+timeout /t 1
+echo 6
+timeout /t 1
+echo 7
+timeout /t 1
+echo 8
+timeout /t 1
+echo 9
+timeout /t 1
+pause
