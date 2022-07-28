c9ee53e1e73d84965b1263d1d951a33bdd9c871f
Grizlik :D <96445475+Grizlikk@users.noreply.github.com>
2022-07-28 22:09:14 +0200

    Create P T.bat

---
+++ Batch/Batch #03 Práce se soubory a složkami/P T.bat
@@ -0,0 +1,19 @@
+@echo off
+echo 1
+timeout /t 1 > nul
+echo 2
+timeout /t 1 > nul
+echo 3
+timeout /t 1 > nul
+echo 4
+timeout /t 1 > nul
+echo 5
+timeout /t 1 > nul
+echo 6
+timeout /t 1 > nul
+echo 7
+timeout /t 1 > nul
+echo 8
+timeout /t 1 > nul
+echo 9
+pause > nul
