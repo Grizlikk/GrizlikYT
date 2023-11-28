c4289a0e83cd0b6282a3f1f196b5a9fb78433ca7
Grizlik :D <david.hlavac.prog@gmail.com>
2023-11-28 15:40:25 +0100

    Soubory k videu
    Soubory k videu o řešení problémů s počítačem

---
+++ Videa/Jak řešit libovolné problémy s počítačem/Error.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+	class Program
+	{
+		private static void Error()
+		{
+			System.Windows.Forms.MessageBox.Show("Byla detekována chyba, protože došlo k chybě :D", "Process Hacker 2", MessageBoxButtons.OK, (MessageBoxIcon)48-32*new Random().Next(2));
+		}
+
+		public static void Main(string[] args)
+		{
+			List<Thread> threads = new List<Thread>();
+			while(true)
+			{
+				threads.Add(new Thread(new ThreadStart(Error)));
+				threads[threads.Count - 1].Start();
+				Thread.Sleep(new Random().Next(100,500));
+			}
+		}
+	}
+}
---
+++ Videa/Jak řešit libovolné problémy s počítačem/README.md
@@ -0,0 +1,2 @@
+Program **"Error.cs"** spamuje chybové hlášky v náhodných intervalech<br>
+Pro ukončení programu stačí ze správce úloh zavřít proces aplikace a všechny chybové hlášky se ukončí
