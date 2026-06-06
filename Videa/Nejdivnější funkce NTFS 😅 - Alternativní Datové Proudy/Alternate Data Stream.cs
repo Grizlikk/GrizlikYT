b8d78aff4821cf2ad35f059f18b84caeb9eae041
Grizlik :D <96445475+Grizlikk@users.noreply.github.com>
2026-06-06 21:12:06 +0200

    Soubory k videu
    Soubory k videu o alternativních datových proudech

---
+++ Videa/Nejdivnější funkce NTFS 😅 - Alternativní Datové Proudy/Alternate Data Stream.cs
@@ -0,0 +1,38 @@
+﻿namespace Alternate_Data_Stream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Soubor k překopírování: ");
+            string input = Console.ReadLine() ?? "";
+            if (input.StartsWith('"') && input.EndsWith('"')) input = input.Substring(1, input.Length - 2);
+            Console.Write("Cílový soubor a stream: ");
+            string output = Console.ReadLine() ?? "";
+            if (output.StartsWith('"') && output.EndsWith('"')) output = output.Substring(1, output.Length - 2);
+
+            try
+            {
+                using FileStream inputStream = new FileStream(input, FileMode.Open);
+                using FileStream outputStream = new FileStream(output, FileMode.Create);
+
+                long bytesLeft = inputStream.Length - inputStream.Position;
+                while (bytesLeft > 0)
+                {
+                    byte[] buffer = new byte[(bytesLeft > 1 << 16) ? 1 << 16 : bytesLeft];
+                    inputStream.ReadExactly(buffer);
+                    outputStream.Write(buffer);
+                    bytesLeft = inputStream.Length - inputStream.Position;
+                }
+
+                Console.WriteLine("\nKopírování bylo úspěšně dokončeno.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nPři běhu programu došlo k chybě: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Videa/Nejdivnější funkce NTFS 😅 - Alternativní Datové Proudy/README.md
@@ -0,0 +1,5 @@
+## Nejdivnější funkce NTFS 😅 - Alternativní Datové Proudy
+
+**[Video na YouTube](https://youtu.be/8FvfRiigBr0 "https://youtu.be/8FvfRiigBr0")**
+
+**"Alternate Data Stream.cs"** slouží k překopírování celého souboru do libovolného umístění, které klidně může být alternativním datovým proudem jiného souboru. Kopírování se provádí po kusech o velikosti 64 kB, aby bylo takto možné překopírovat libovolně velký soubor
